7bc22d55cb22d594e0b05c243d4d47b3c3a10320
Eddy DREGER (ITT) <eddy.dreger@intimetec.com>
2021-05-04 02:44:14 +0000

    TO DO

--- src/Program.cs
+++ src/Program.cs
@@ -172,6 +172,7 @@ namespace learn_and_code
             return (input.Aggregate(0UL,
                                     // XXX: TO DO - validate input character "c"
                                     (a, c) => (a << 8) | (FacetValueBase << (int)(c - '0')),
+                                    // XXX: TO DO - no transforms beyond typecast / bit-bang elsewhere
                                     u => (FacetValue)(u ^ (u >> 4) ^ MagicXorMask)));
         }
 
